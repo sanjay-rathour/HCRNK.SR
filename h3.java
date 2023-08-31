16b91e0e5e5125762e251d7ff7aee542e0ebb5a8
Sanjay Rathour <117800972+sanjayrathou@users.noreply.github.com>
2023-08-31 15:59:46 +0530

    Create h3.java

---
+++ h3.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+	
+    public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+        
+        int j = scan.nextInt();
+        double e = scan.nextDouble();
+        scan.nextLine();
+        String t = scan.nextLine();
+        
+        System.out.println(i + j);
+        System.out.println(d + e);
+        System.out.println(s + t);
+        
+
+        /* Declare second integer, double, and String variables. */
+
+        /* Read and save an integer, double, and String to your variables.*/
+        // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        
+        /* Print the sum of both integer variables on a new line. */
+
+        /* Print the sum of the double variables on a new line. */
+		
+        /* Concatenate and print the String variables on a new line; 
+        	the 's' variable above should be printed first. */
+
+        scan.close();
+    }
+}
