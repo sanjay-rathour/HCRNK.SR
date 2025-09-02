a2a4263378325b5687f3b0a5b4941a235d7493d5
Sanjay Rathour <117800972+sanjay-rathour@users.noreply.github.com>
2025-09-02 21:34:30 +0530

    Update h3.java

--- h3.java
+++ h3.java
@@ -6,7 +6,7 @@ import java.util.regex.*;
 
 public class Solution {
 	 
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
         int i = 4;
         double d = 4.0;
         String s = "HackerRank ";  
