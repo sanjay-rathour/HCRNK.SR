5d13f58cf62d1b5daefe6feeb2528b3475bdae38
Sanjay Rathour <117800972+sanjay-rathour@users.noreply.github.com>
2025-09-28 22:50:27 +0530

    Update h3.java

--- h3.java
+++ h3.java
@@ -27,7 +27,7 @@ public class Solution {
         /* Read and save an integer, double, and String to your variables.*/
         // Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
         
-        /* Print the sum of both integer variables on a new line. */
+        /* Print the sum of both integer variables on a new line. */ 
 
         /* Print the sum of the double variables on a new line. */
 		
