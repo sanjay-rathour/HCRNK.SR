2659673185cb57ef19cd444c688a617f7e09009d
Sanjay Rathour <117800972+sanjay-rathour@users.noreply.github.com>
2025-12-15 22:26:12 +0530

    Update h3.java

--- h3.java
+++ h3.java
@@ -8,7 +8,7 @@ public class Solution {
 	 
     public static void main(String[] args) { 
         int i = 4;
-        double d = 4.0;
+        double d = 4.0; 
         String s = "HackerRank ";  
         Scanner scan = new Scanner(System.in);
 
